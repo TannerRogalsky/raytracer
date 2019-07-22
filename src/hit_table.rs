92f708c6cf388f0b6893ba90990e2e734377b77b
Tanner Rogalsky <tanner@tannerrogalsky.com>
2019-07-22 08:20:47 -0400

    wip hit table typing

--- src/hit_table.rs
+++ src/hit_table.rs
@@ -1,6 +1,6 @@
 extern crate cgmath;
 
-use cgmath::Vector3;
+use cgmath::{Vector3, Zero};
 
 pub struct HitRecord<T> {
     t: f64,
@@ -34,6 +34,48 @@ impl<T> HitRecord<T> {
     }
 }
 
-pub trait HitTable<T> {
-    fn hit(&self, r: &super::ray::Ray<T>, t: std::ops::Range<T>, rec: &mut HitRecord<T>) -> bool;
+impl<T: cgmath::BaseNum> Default for HitRecord<T> {
+    fn default() -> Self {
+        Self {
+            t: 0.0,
+            p: Vector3::zero(),
+            normal: Vector3::zero(),
+        }
+    }
 }
+
+pub trait HitTable<T> {
+    type Vector3 = cgmath::Vector3<T>;
+
+    fn hit(&self, r: &super::ray::Ray<T>, t: std::ops::Range<f64>, rec: &mut HitRecord<T>) -> bool;
+}
+
+struct HitTableList<T> {
+    list: std::vec::Vec<Box<HitTable<T>>>,
+}
+
+//impl<T: cgmath::BaseNum> HitTableList<T> {
+//    pub fn new() -> Self {
+//        HitTableList { list: vec![] }
+//    }
+//
+//    pub fn hit(
+//        &self,
+//        r: &super::ray::Ray<T>,
+//        t: std::ops::Range<f64>,
+//        rec: &mut HitRecord<T>,
+//    ) -> bool {
+//        let mut temp_rec = HitRecord::default();
+//        let mut hit_anything = false;
+//        let mut closest_so_far = t.end;
+//
+//        for hit_table in self.list {
+//            if hit_table.hit(r, t.start..closest_so_far, &mut temp_rec) {
+//                hit_anything = true;
+//                closest_so_far = temp_rec.get_t();
+//                *rec = temp_rec;
+//            }
+//        }
+//        hit_anything
+//    }
+//}
--- src/sphere.rs
+++ src/sphere.rs
@@ -1,46 +1,46 @@
 extern crate cgmath;
 
-use std::ops::Range;
-use super::*;
+use super::{HitRecord, HitTable, Ray};
 use cgmath::InnerSpace;
+use std::ops::Range;
 
 struct Sphere<T> {
     center: cgmath::Vector3<T>,
-    radius: f64,
+    radius: T,
 }
 
-//impl<T: cgmath::BaseNum> HitTable<T> for Sphere<T> {
-//    fn hit(&self, r: &Ray<T>, t: Range<T>, rec: &mut HitRecord<T>) {
+impl<T: cgmath::BaseFloat> HitTable<T> for Sphere<T> {
+    fn hit(&self, r: &Ray<T>, t: Range<f64>, rec: &mut HitRecord<T>) -> bool where T: cgmath::BaseFloat, cgmath::Vector3<T>: cgmath::InnerSpace {
+//        let oc = r.origin() - self.center;
+//        let a = r.direction().dot(r.direction().to_owned());
+        let a = r.direction().magnitude2();
+        true
+    }
+}
+
+//impl HitTable<f64> for Sphere<f64> {
+//    fn hit(&self, r: &Ray<f64>, t: Range<f64>, rec: &mut HitRecord<f64>) -> bool {
 //        let oc = r.origin() - self.center;
 //        let a = r.direction().dot(r.direction().to_owned());
 //        let b = 2.0 * oc.dot(r.direction().to_owned());
 //        let c = oc.dot(oc) - self.radius * self.radius;
+//        let discriminant = b * b - 4.0 * a * c;
+//        if discriminant > 0.0 {
+//            let temp = (-b - (b * b - a * c).sqrt()) / a;
+//            if t.contains(&temp) {
+//                rec.set_t(temp);
+//                rec.set_p(r.point_at_parameter(rec.get_t()));
+//                rec.set_normal((rec.get_p() - self.center) / self.radius);
+//                return true;
+//            }
+//            let temp = (-b + (b * b - a * c).sqrt()) / a;
+//            if t.contains(&temp) {
+//                rec.set_t(temp);
+//                rec.set_p(r.point_at_parameter(rec.get_t()));
+//                rec.set_normal((rec.get_p() - self.center) / self.radius);
+//                return true;
+//            }
+//        }
+//        false
 //    }
 //}
-
-impl HitTable<f64> for Sphere<f64> {
-    fn hit(&self, r: &Ray<f64>, t: Range<f64>, rec: &mut HitRecord<f64>) -> bool {
-        let oc = r.origin() - self.center;
-        let a = r.direction().dot(r.direction().to_owned());
-        let b = 2.0 * oc.dot(r.direction().to_owned());
-        let c = oc.dot(oc) - self.radius * self.radius;
-        let discriminant = b * b - 4.0 * a * c;
-        if discriminant > 0.0 {
-            let temp = (-b - (b * b - a * c).sqrt()) / a;
-            if t.contains(&temp) {
-                rec.set_t(temp);
-                rec.set_p(r.point_at_parameter(rec.get_t()));
-                rec.set_normal((rec.get_p() - self.center) / self.radius);
-                return true;
-            }
-            let temp = (-b + (b * b - a * c).sqrt()) / a;
-            if t.contains(&temp) {
-                rec.set_t(temp);
-                rec.set_p(r.point_at_parameter(rec.get_t()));
-                rec.set_normal((rec.get_p() - self.center) / self.radius);
-                return true;
-            }
-        }
-        false
-    }
-}
